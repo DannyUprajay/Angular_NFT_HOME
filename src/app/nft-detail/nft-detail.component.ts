4980e3a78249a80364886447575247f5292cd7e7
danny <danny.uprajay@gmail.com>
2023-10-16 23:58:37 +0200

    ajout du edit pour le nft

--- src/app/nft-detail/nft-detail.component.ts
+++ src/app/nft-detail/nft-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NftInterface} from "../nft.interface";
 import {NftService} from "../services/nft.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Observable} from "rxjs";
 import {NftComponent} from "../nft/nft.component";
 import {FormControl, FormGroup} from "@angular/forms";
@@ -16,8 +16,14 @@ export class NftDetailComponent implements OnInit{
 
     nft: NftInterface | undefined;
 
+  public editNft: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    pathImage: new FormControl(''),
+    price: new FormControl(''),
+  });
 
   constructor(
+    private router: Router,
     private nftService: NftService,
     private route: ActivatedRoute,
     private auth: AuthService
@@ -25,12 +31,34 @@ export class NftDetailComponent implements OnInit{
 
 
     ngOnInit() {
-        this.route.params.subscribe(params => {
-            let nftId = +params['id'];
-            this.getNftId(nftId);
-        });
+      this.route.params.subscribe(params => {
+        let nftId = +params['id'];
+        this.getNftId(nftId);
+      });
+
+
+      this.nftService.getNftById(this.route.snapshot.params['id']).subscribe((result) => {
+        console.log(result);
+        this.editNft = new FormGroup({
+          name: new FormControl(result['name']),
+          pathImage: new FormControl(result['pathImage']),
+          price: new FormControl(result['price']),
+        })
+
+        // this.auth.clearToken();
+        this.auth.getToken()
+        // this.nftService.getUserData();
+
+      });
     }
 
+
+  update() {
+    this.nftService.updateNft(this.route.snapshot.params['id'], this.editNft.value).subscribe((result) => {
+      console.log(result);
+      this.router.navigate(['/nft/' + this.route.snapshot.params['id']]);
+    })
+  }
     getNftId(id: number) {
         this.nftService.getNftById(id).subscribe(nftResult => {
             this.nft = nftResult;
